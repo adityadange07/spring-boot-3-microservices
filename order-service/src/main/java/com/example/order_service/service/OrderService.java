51e9763a5950821f7c4276a982784377ef031478
Aditya Dange <adityadange1@gmail.com>
2025-11-18 08:59:06 +0530

    Update

---
+++ order-service/src/main/java/com/example/order_service/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.example.order_service.service;
+
+import com.example.order_service.dto.OrderRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public void placeOrder(OrderRequest orderRequest) {
+
+    }
+
+}
